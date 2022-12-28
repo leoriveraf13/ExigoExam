852931704571adc9d3adcc2625dca79857db333d
leo_rivflo <leo.rivera@rank.com>
2022-12-28 16:46:30 -0600

    First commit for Exigo Exam

---
+++ ExigoExam/Controllers/JoinController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ExigoExam.Models;
+
+namespace ExigoExam.Controllers
+{
+    public class JoinController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new CandidateModel();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(CandidateModel model)
+        {
+            return View("Index", model);
+        }
+    }
+}
---
+++ ExigoExam/Models/CandidateModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExigoExam.Models
+{
+    public enum Positions
+    {
+        Web, DBA, Bussiness_Analyst
+    }
+    
+    public class CandidateModel
+    {
+        private bool positionValidate(object value)
+        {
+            return Enum.IsDefined(typeof(Positions), value);
+        }
+
+        [Required(ErrorMessage = "The field First Name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "The field Last Name is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "The field Position is required")]
+        public Positions Position { get; set; }
+
+        [Required(ErrorMessage = "The field Joining Date is required")]
+        public DateTime JoiningDate { get; set; }
+
+        [Required(ErrorMessage = "The field Salary is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "The field Salary must be greater than 0.")]
+        public int Salary { get; set; }
+        public bool OtherAplies { get; set; }
+    }
+}
